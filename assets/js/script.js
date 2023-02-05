14609d5b2032aac46adc954bcef9baff87c5cbe0
kamel beloula <kamel.solo@live.com>
2023-02-05 20:17:58 +0000

    added a translator midule from openpen website, to break the language barrier

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,7 +15,7 @@ function persistData() {
     for (i = 0; i < localStorage.length; i++) {
         var historyBtn = $('<button>').addClass("history-btn").addClass("btn").addClass("btn-outline-primary").css({ border: "#f6f6f6", margin: "5px", padding: "4px" })
         var histText = localStorage.getItem("destination" + [i])
-        historyBtn.text(histText.charAt(0).toUpperCase() + histText.slice(1))
+        historyBtn.text(histText)
         $('#hist-buttons').append(historyBtn)
     }
 }
@@ -240,7 +240,7 @@ function moreDetails(response) {
     var lon = response.lon
     var lat = response.lat
 
-    var filtersURL = "https://api.opentripmap.com/0.1/en/places/radius?radius=" + radius + "&lon=" + lon + "&lat=" + lat + "&rate=3&format=json&limit=4&apikey=" + apiKey
+    var filtersURL = "https://api.opentripmap.com/0.1/en/places/radius?radius=" + radius + "&lon=" + lon + "&lat=" + lat + "&rate=3&format=json&limit=6&apikey=" + apiKey
 
     $.ajax({
         url: filtersURL,
@@ -248,7 +248,7 @@ function moreDetails(response) {
     }).then(function (filterResults) {
         console.log(filterResults)
 
-        topAttractions(filterResults)
+        pointsOfInterest(filterResults)
     })
 }
 
@@ -291,7 +291,7 @@ function pointsOfInterest(filterResults) {
                 // ($("<a>").attr("target",_blank).attr("href",wikipedia).text("See more at Wikipedia.com"))
                 $("<p>")
                 //opentripmap code example for 
-                .attr("class", "card-text").innerHTML = `<p><a target="_blank" href="${data.wikipedia}">Show more at Wikipedia</a></p>`
+                .attr("class", "card-text").innerHTML = `<p><a target="_blank" href="${data.wikipedia}">Full article at Wikipedia.com</a></p>`
                 // ($("<a>").attr("target",_blank).attr("href",wikipedia).text("See more at Wikipedia.com"))
               ])
             )
@@ -331,7 +331,6 @@ function loadImg() {
 //end unsplash
 
 // KAMEL Wiki
-window.addEventListener("load", infos);
 function infos() {
   const url =
     "https://en.wikipedia.org/w/api.php?format=json&action=query&origin=*&prop=extracts&exchars=1000&exintro=true&explaintext=true&generator=search&gsrlimit=3&gsrsearch=" +
@@ -348,3 +347,209 @@ function infos() {
     });
 }
 // end wiki
+
+
+//translator start
+//https://codepen.io/oussamasibari/pen/JjLLxxv
+const countries = {
+  "am-ET": "Amharic",
+  "ar-SA": "Arabic",
+  "be-BY": "Bielarus",
+  "bem-ZM": "Bemba",
+  "bi-VU": "Bislama",
+  "bjs-BB": "Bajan",
+  "bn-IN": "Bengali",
+  "bo-CN": "Tibetan",
+  "br-FR": "Breton",
+  "bs-BA": "Bosnian",
+  "ca-ES": "Catalan",
+  "cop-EG": "Coptic",
+  "cs-CZ": "Czech",
+  "cy-GB": "Welsh",
+  "da-DK": "Danish",
+  "dz-BT": "Dzongkha",
+  "de-DE": "German",
+  "dv-MV": "Maldivian",
+  "el-GR": "Greek",
+  "en-GB": "English",
+  "es-ES": "Spanish",
+  "et-EE": "Estonian",
+  "eu-ES": "Basque",
+  "fa-IR": "Persian",
+  "fi-FI": "Finnish",
+  "fn-FNG": "Fanagalo",
+  "fo-FO": "Faroese",
+  "fr-FR": "French",
+  "gl-ES": "Galician",
+  "gu-IN": "Gujarati",
+  "ha-NE": "Hausa",
+  "he-IL": "Hebrew",
+  "hi-IN": "Hindi",
+  "hr-HR": "Croatian",
+  "hu-HU": "Hungarian",
+  "id-ID": "Indonesian",
+  "is-IS": "Icelandic",
+  "it-IT": "Italian",
+  "ja-JP": "Japanese",
+  "kk-KZ": "Kazakh",
+  "km-KM": "Khmer",
+  "kn-IN": "Kannada",
+  "ko-KR": "Korean",
+  "ku-TR": "Kurdish",
+  "ky-KG": "Kyrgyz",
+  "la-VA": "Latin",
+  "lo-LA": "Lao",
+  "lv-LV": "Latvian",
+  "men-SL": "Mende",
+  "mg-MG": "Malagasy",
+  "mi-NZ": "Maori",
+  "ms-MY": "Malay",
+  "mt-MT": "Maltese",
+  "my-MM": "Burmese",
+  "ne-NP": "Nepali",
+  "niu-NU": "Niuean",
+  "nl-NL": "Dutch",
+  "no-NO": "Norwegian",
+  "ny-MW": "Nyanja",
+  "ur-PK": "Pakistani",
+  "pau-PW": "Palauan",
+  "pa-IN": "Panjabi",
+  "ps-PK": "Pashto",
+  "pis-SB": "Pijin",
+  "pl-PL": "Polish",
+  "pt-PT": "Portuguese",
+  "rn-BI": "Kirundi",
+  "ro-RO": "Romanian",
+  "ru-RU": "Russian",
+  "sg-CF": "Sango",
+  "si-LK": "Sinhala",
+  "sk-SK": "Slovak",
+  "sm-WS": "Samoan",
+  "sn-ZW": "Shona",
+  "so-SO": "Somali",
+  "sq-AL": "Albanian",
+  "sr-RS": "Serbian",
+  "sv-SE": "Swedish",
+  "sw-SZ": "Swahili",
+  "ta-LK": "Tamil",
+  "te-IN": "Telugu",
+  "tet-TL": "Tetum",
+  "tg-TJ": "Tajik",
+  "th-TH": "Thai",
+  "ti-TI": "Tigrinya",
+  "tk-TM": "Turkmen",
+  "tl-PH": "Tagalog",
+  "tn-BW": "Tswana",
+  "to-TO": "Tongan",
+  "tr-TR": "Turkish",
+  "uk-UA": "Ukrainian",
+  "uz-UZ": "Uzbek",
+  "vi-VN": "Vietnamese",
+  "wo-SN": "Wolof",
+  "xh-ZA": "Xhosa",
+  "yi-YD": "Yiddish",
+  "zu-ZA": "Zulu"
+}
+
+const fromText = document.querySelector(".from-text"),
+toText = document.querySelector(".to-text"),
+exchageIcon = document.querySelector(".exchange"),
+selectTag = document.querySelectorAll("select"),
+icons = document.querySelectorAll(".row i");
+translateBtn = document.querySelector("#translateBtn"),
+
+selectTag.forEach((tag, id) => {
+  for (let country_code in countries) {
+      let selected = id == 0 ? country_code == "en-GB" ? "selected" : "" : country_code == "es-ES" ? "selected" : "";
+      let option = `<option ${selected} value="${country_code}">${countries[country_code]}</option>`;
+        tag.insertAdjacentHTML("beforeend", option);
+  }
+});
+
+exchageIcon.addEventListener("click", () => {
+  let tempText = fromText.value,
+  tempLang = selectTag[0].value;
+  fromText.value = toText.innerText;
+  toText.innerText = tempText;
+  selectTag[0].value = selectTag[1].value;
+  selectTag[1].value = tempLang;
+});
+
+fromText.addEventListener("keyup", () => {
+  if(!fromText.value) {
+      toText.innerText = "";
+  }
+});
+
+/****************************/
+const options = {
+method: 'POST',
+headers: {
+  'content-type': 'application/json',
+  'X-RapidAPI-Key': 'c2432b444dmsh909f603234f0c69p1a0e09jsne64b3c285be3',
+  'X-RapidAPI-Host': 'ultra-fast-translation.p.rapidapi.com'
+},
+body: '{"from":"auto","to":"ar","e":"","q":""'
+};
+/****************************/
+
+translateBtn.addEventListener("click", () => {
+  let text = fromText.value.trim(),
+  translateFrom = selectTag[0].value,
+  translateTo = selectTag[1].value;
+  if(!text) return;
+  toText.setAttribute("placeholder", "Translating...");
+  translateBtn.innerText = "Translating...";
+  options.body =JSON.stringify(
+    {
+      "from":translateFrom,
+      "to":translateTo,
+      "e":"",
+      "q":text.split('\n')
+    }
+  );
+  let apiUrl = `https://ultra-fast-translation.p.rapidapi.com/t`;
+  fetch(apiUrl, options).then(res => res.json()).then(data => {
+
+      //console.log(options.body);
+      //console.log(data);
+
+      if(data.message){
+        toText.style.color = "red";
+        toText.innerHTML = data.message;
+        return
+      }else{
+        toText.style.color = "black";
+        toText.innerHTML = data.map(e => (e||e[0])).join('<br/>');
+      }
+      
+      toText.setAttribute("placeholder", "Translation");
+      translateBtn.innerText = "Translate Text";
+  });
+});
+
+
+
+icons.forEach(icon => {
+  icon.addEventListener("click", ({target}) => {
+      if(!fromText.value || !toText.innerText) return;
+      if(target.classList.contains("fa-copy")) {
+          if(target.id == "from") {
+              navigator.clipboard.writeText(fromText.value);
+          } else {
+              navigator.clipboard.writeText(toText.innerText);
+          }
+      } else {
+          let utterance;
+          if(target.id == "from") {
+              utterance = new SpeechSynthesisUtterance(fromText.value);
+              utterance.lang = selectTag[0].value;
+          } else {
+              utterance = new SpeechSynthesisUtterance(toText.innerText);
+              utterance.lang = selectTag[1].value;
+          }
+          speechSynthesis.speak(utterance);
+      }
+  });
+});
+//translator end
