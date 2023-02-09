27c5248f934c598052610a602f90203ea5887739
reets <117296082+reets93@users.noreply.github.com>
2023-02-09 00:39:33 +0000

    updated remaining http to https to fix 'mixed content' error

--- assets/js/script.js
+++ assets/js/script.js
@@ -88,7 +88,7 @@ $("#random-btn").on("click", function (e) {
 
   destination = surpriseCity;
   var destURL =
-    "http://api.opentripmap.com/0.1/en/places/geoname?name=" +
+    "https://api.opentripmap.com/0.1/en/places/geoname?name=" +
     destination +
     "&apikey=" +
     "5ae2e3f221c38a28845f05b6b0c68e4cbb10ed5f2dbed753f3070329";
